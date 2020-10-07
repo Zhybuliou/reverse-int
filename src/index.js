7d44291bf9ce09846a626513b9744cbc5186e3be
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-10-07 13:59:38 +0300

    docs: done task reerse- int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){
+        let res = (n * -1);
+        return res.toString().split('').reverse().join('');
+    }else{
+   return n.toString().split('').reverse().join('');
+    }
 }
